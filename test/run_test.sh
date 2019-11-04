10489e7c08be535a402abb8fb8f61728db751ffe
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2019-11-04 04:21:21 +0000

    refactor: move build directory with gcc

--- test/run_test.sh
+++ test/run_test.sh
@@ -20,17 +20,17 @@
 cd $(dirname $0)
 
 echo ""
-echo ">> build with cmake"
+echo ">> build with cmake and gcc"
 echo ""
 rm -rf build
-mkdir build
-cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+mkdir -p build/gcc
+cd build/gcc
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../..
 cmake --build .
 ctest -V .
 
 echo ""
-echo ">> collect coverage"
+echo ">> collect coverage with lcov (and gcov)"
 echo ""
 COV=./coverage/coverage.info
 HTML=./coverage/html
@@ -42,7 +42,7 @@ lcov --rc lcov_branch_coverage=1 --remove $COV "*/Test/*" --output-file $COV
 lcov --rc lcov_branch_coverage=1 --list $COV
 genhtml --rc lcov_branch_coverage=1 --output-directory $HTML $COV
 
-cd ../
+cd ../..
 echo ""
 echo ">> PlantUML"
 echo ""
@@ -58,7 +58,7 @@ echo ""
 echo ">> clang-tidy"
 echo ""
 mkdir build/clang-tidy
-clang-tidy -checks=* -p=build/compile_commands.json src/add.cpp \
+clang-tidy -checks=* -p=build/gcc/compile_commands.json src/add.cpp \
     | tee build/clang-tidy/clang-tidy.log
 
 echo ""
